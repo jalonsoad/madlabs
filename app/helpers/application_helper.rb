1b8dc064eb56cbcbff394b31cce42bbf93acb2ea
Julio Cesar Alonso <juliocesar.alonso@gmail.com>
2011-02-01 10:25:14 +0100

    new

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,4 @@
+module ApplicationHelper
+  include TweetButton
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madlabs::Application.config.secret_token = '9e33e1823c1bd65071fe2d6fdf7f4e03dba1598d639b1271a8ed89fcf3c9cc11761a2de106015702d22d79ce4a6bf30b608abc6ca23570462d0ed1dfac142683'
